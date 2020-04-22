79ebf6d0e986fdc6c902b6fece1da07e5b858d85
Tomoko Yuki <tomoko.monzen@rakuten.com>
2020-04-22 10:35:44 +0900

    Added - Track

--- src/Track/index.jsx
+++ src/Track/index.jsx
@@ -8,5 +8,20 @@ export default function Index() {
       Component: lazy(() => import('./Default')),
       maxWidth: '100%',
     },
+    {
+      name: 'Four Steps',
+      Component: lazy(() => import('./FourSteps')),
+      maxWidth: '100%',
+    },
+    {
+      name: 'Three Steps',
+      Component: lazy(() => import('./ThreeSteps')),
+      maxWidth: '100%',
+    },
+    {
+      name: 'Theme',
+      Component: lazy(() => import('./Theme')),
+      maxWidth: '100%',
+    },
   ]);
 }
