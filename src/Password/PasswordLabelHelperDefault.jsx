7de2e3a7063f942696baeaa70ef18b623606f2c8
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-04-22 12:09:31 +0900

    Added - Password

---
+++ src/Password/PasswordLabelHelperDefault.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PasswordLabelHelper from '@rakuten-rex/password';
+
+export default function MyComponent() {
+  return (
+    <PasswordLabelHelper
+      placeholder='Password'
+      htmlFor='password'
+      label='Label'
+      labelId='labelId'
+      helper='Helper Text'
+      name='Password'
+    />
+  );
+}
