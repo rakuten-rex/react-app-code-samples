f0d033d47c312cb8ca5a49f5fe266e6c66a1b4cb
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-04-22 10:53:06 +0900

    Added - Textarea

--- src/Textarea/index.jsx
+++ src/Textarea/index.jsx
@@ -7,5 +7,41 @@ export default function Index() {
       name: 'Default',
       Component: lazy(() => import('./Default'))
     },
+    {
+      name: 'Disabled',
+      Component: lazy(() => import('./Disabled'))
+    },
+    {
+      name: 'Error',
+      Component: lazy(() => import('./Error'))
+    },
+    {
+      name: 'Valid',
+      Component: lazy(() => import('./Valid'))
+    },
+    {
+      name: 'Theme',
+      Component: lazy(() => import('./Theme'))
+    },
+    {
+      name: 'Textarea Label Helper Default',
+      Component: lazy(() => import('./TextareaLabelHelperDefault'))
+    },
+    {
+      name: 'Textarea Label Helper Disabled',
+      Component: lazy(() => import('./TextareaLabelHelperDisabled'))
+    },
+    {
+      name: 'Textarea Label Helper Error',
+      Component: lazy(() => import('./TextareaLabelHelperError'))
+    },
+    {
+      name: 'Textarea Label Helper Valid',
+      Component: lazy(() => import('./TextareaLabelHelperValid'))
+    },
+    {
+      name: 'Textarea Label Helper Theme',
+      Component: lazy(() => import('./TextareaLabelHelperTheme'))
+    },
   ]);
 }
