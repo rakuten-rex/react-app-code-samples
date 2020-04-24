b7f770fb8779ca68cbfb670c0333e30457724fc5
Tomoko Yuki <tomoko.monzen@rakuten.com>
2020-04-24 15:51:37 +0900

    Added file: to index

--- src/Panel/index.jsx
+++ src/Panel/index.jsx
@@ -2,10 +2,30 @@ import { lazy } from 'react';
 import Page from '../Page';
 
 export default function Index() {
-	return Page('Panel', [
-		{
-			name: 'Default',
-			Component: lazy(() => import('./Default'))
-		}
-	]);
+  return Page('Panel', [
+    {
+      name: 'Default',
+      Component: lazy(() => import('./Default')),
+      maxWidth: '100%',
+      file: 'Default',
+    },
+    {
+      name: 'With Box Shadow',
+      Component: lazy(() => import('./WithBoxShadow')),
+      maxWidth: '90%',
+      file: 'WithBoxShadow',
+    },
+    {
+      name: 'With Box Shadow And Padding',
+      Component: lazy(() => import('./WithBoxShadowAndPadding')),
+      maxWidth: '90%',
+      file: 'WithBoxShadowAndPadding',
+    },
+    {
+      name: 'Theme',
+      Component: lazy(() => import('./Theme')),
+      maxWidth: '90%',
+      file: 'Theme',
+    },
+  ]);
 }
--- src/Stepper/index.jsx
+++ src/Stepper/index.jsx
@@ -7,41 +7,49 @@ export default function Index() {
       name: 'Stepper Default',
       Component: lazy(() => import('./Default')),
       maxWidth: '15%',
+      file: 'Default',
     },
     {
       name: 'Stepper Disabled State',
       Component: lazy(() => import('./DisabledState')),
       maxWidth: '15%',
+      file: 'DisabledState',
     },
     {
       name: 'Stepper With Default Value',
       Component: lazy(() => import('./WithDefaultValue')),
       maxWidth: '15%',
+      file: 'WithDefaultValue',
     },
     {
       name: 'Stepper Theme',
       Component: lazy(() => import('./Theme')),
       maxWidth: '15%',
+      file: 'Theme',
     },
     {
       name: 'Stepper Label Default',
       Component: lazy(() => import('./LabelDefault')),
       maxWidth: '15%',
+      file: 'LabelDefault',
     },
     {
       name: 'Stepper Label Disabled',
       Component: lazy(() => import('./LabelDisabled')),
       maxWidth: '15%',
+      file: 'LabelDisabled',
     },
     {
       name: 'Stepper Label With Default Value',
       Component: lazy(() => import('./LabelWithDefaultValue')),
       maxWidth: '15%',
+      file: 'LabelWithDefaultValue',
     },
     {
       name: 'Stepper Label Theme',
       Component: lazy(() => import('./LabelTheme')),
       maxWidth: '15%',
+      file: 'LabelTheme',
     },
   ]);
 }
