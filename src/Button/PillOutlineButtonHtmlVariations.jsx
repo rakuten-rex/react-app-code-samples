4145e4f0e372ae628bd4ce1c2bbef8dc369bdbe9
Claudio <claudio.moya@rakuten.com>
2020-04-28 14:38:27 +0900

    Update - Button variations

--- src/Button/PillOutlineButtonHtmlVariations.jsx
+++ src/Button/PillOutlineButtonHtmlVariations.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PillOutlineButton from '@rakuten-rex/button/PillOutlineButton';
 import PillOutlineButtonInput from '@rakuten-rex/button/PillOutlineButton/PillOutlineButtonDiv';
 import PillOutlineButtonLink from '@rakuten-rex/button/PillOutlineButton/PillOutlineButtonLink';
 import PillOutlineButtonDiv from '@rakuten-rex/button/PillOutlineButton/PillOutlineButtonDiv';
@@ -6,6 +7,7 @@ import PillOutlineButtonDiv from '@rakuten-rex/button/PillOutlineButton/PillOutl
 export default function MyComponent() {
   return (
     <div>
+      <PillOutlineButton>Button Tag</PillOutlineButton>
       <PillOutlineButtonInput>Input Tag</PillOutlineButtonInput>{' '}
       <PillOutlineButtonLink href="/">Link Tag with href</PillOutlineButtonLink>{' '}
       <PillOutlineButtonDiv>Div Tag</PillOutlineButtonDiv>
